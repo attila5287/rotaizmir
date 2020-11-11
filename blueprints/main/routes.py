1306b3df82852cf46a521188a98c2bc17655a8a3
attila5287 <atiturkoz@hotmail.com>
2020-11-11 05:19:14 -0700

    sser

--- blueprints/main/routes.py
+++ blueprints/main/routes.py
@@ -7,38 +7,38 @@ main = Blueprint('main', __name__)
 @main.route("/")
 @main.route("/about")
 def about():
-    pass
+  pass
+  return render_template(
+    'about.html',
+    info_notes=[
+      'General instructions for guests',
+    ],
+    css=[
+      ('theme', '/minty/bootstrap', ),
+      ('main', 'main', ),
+    ],
+    js=[
+      ('about', 'sunburst', ),
+    ],
+    legend='About App',
+    title='About',
+    access=[
+      'u',
+      'm',
+      'a',
+      'p',
+    ],
+  )
 
-    return render_template('about.html',
-                           info_notes=[
-                            'General instructions for guests',
-                            ],
-                            access=[
-                                'u',
-                                'm',
-                                'a',
-                                'p',
-                            ],
-                            css=[
-                                ('theme', '/minty/bootstrap', ),
-                                ('main', 'main', ),
-                            ],
-                            js = [
-                                ('about', 'sunburst', ),
-                            ],        
-                            legend='About App',
-                            title='About',
-                            )
 
-
-@ main.route("/home")
+@main.route("/home")
 def home():
-    pass
-    page = request.args.get('page', 1, type=int)
-    posts = Post.query.order_by(
-        Post.date_posted.desc()).paginate(page=page, per_page=5)
+  pass
+  page = request.args.get('page', 1, type=int)
+  posts = Post.query.order_by(
+    Post.date_posted.desc()).paginate(page=page, per_page=5)
 
-    return render_template('home.html', posts=posts,
+  return render_template('home.html', posts=posts,
                            info_notes=[
                                'Follow public announcements, events.',
                            ],
