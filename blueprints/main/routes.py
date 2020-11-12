e4b91b2f9f44a5e7d0ceb84ae376277c2a646e61
attila5287 <atiturkoz@hotmail.com>
2020-11-12 01:45:03 -0700

    Welcome

--- blueprints/main/routes.py
+++ blueprints/main/routes.py
@@ -7,38 +7,37 @@ main = Blueprint('main', __name__)
 @main.route("/")
 @main.route("/about")
 def about():
-  pass
-  return render_template(
-    'about.html',
-    info_notes=[
-      'General instructions for guests',
-    ],
-    css=[
-      ('theme', '/minty/bootstrap', ),
-      ('main', 'main', ),
-    ],
-    js=[
-      ('about', 'sunburst', ),
-    ],
-    legend='About App',
-    title='About',
-    access=[
-      'u',
-      'm',
-      'a',
-      'p',
-    ],
-  )
+    pass
+    return render_template(
+        'about.html',
+        info_notes=[
+            'General instructions for guests',
+        ],
+        css=[
+            ('theme', '/minty/bootstrap', ),
+            ('main', 'main', ),
+        ],
+        js=[
+            ('about', 'sunburst', ),
+        ],
+        legend='About App',
+        title='About',
+        access=[
+            'u',
+            'm',
+            'a',
+            'p',
+        ],
+    )
 
 
 @main.route("/home")
 def home():
-  pass
-  page = request.args.get('page', 1, type=int)
-  posts = Post.query.order_by(
-    Post.date_posted.desc()).paginate(page=page, per_page=5)
-
-  return render_template('home.html', posts=posts,
+    pass
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(
+        Post.date_posted.desc()).paginate(page=page, per_page=5)
+    return render_template('home.html', posts=posts,
                            info_notes=[
                                'Follow public announcements, events.',
                            ],
@@ -56,3 +55,5 @@ def home():
                            legend='Public Posts',
                            title='Home | Public',
                            )
+
+
---
+++ blueprints/static/about/sunburst.js
@@ -0,0 +1,59 @@
+$( document ).ready( () => {
+  
+  console.log( 'test :>> ' );
+  sunBurnMeUp();
+  
+  function sunBurnMeUp () {
+    var labels = ['guest', 'user', 'member', 'admin'];
+    var parents = ["", "guest", "user", "member", "admin"];
+    var data = [{
+      type: "sunburst",
+      labels: labels,
+      parents: parents,
+      values: [54, 24, 12, 12, 6, 6],
+      outsidetextfont: {
+        size: 12,
+        color: "#377eb8"
+      },
+      leaf: {
+        opacity: 0.4
+      },
+      marker: {
+        line: {
+          width: 2
+        }
+      },
+    }];
+    var layout = {
+      plot_bgcolor: "rgb(255,255,255,0)",
+      paper_bgcolor: "rgb(255,255,255,0)",
+      responsive: true,
+      margin: {
+        l: 0,
+        r: 0,
+        b: 0,
+        t: 0,
+      },
+
+      padding: {
+        l: 0,
+        r: 0,
+        b: 0,
+        t: 0,
+      },
+
+      sunburstcolorway: [
+        "#78c2ad",
+        "#00ddc9",
+        "#B58900",
+        "#636efa",
+        "#EF553B",
+        "#6610f2",
+        "#6f42c1",
+      ],
+      extendsunburstcolorway: true,
+    };
+    Plotly.newPlot('sunburst', data, layout);
+  }
+  
+});
