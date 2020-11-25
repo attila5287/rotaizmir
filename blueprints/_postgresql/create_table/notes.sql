f66adcb7deec1072cdb4ae5b373e7c17301f905c
attila5287 <atiturkoz@hotmail.com>
2020-11-25 12:45:45 -0700

    up notes

--- blueprints/_postgresql/create_table/notes.sql
+++ blueprints/_postgresql/create_table/notes.sql
@@ -1,12 +1,14 @@
+DROP TABLE public.note CASCADE;
 CREATE TABLE note(
 	id BIGSERIAL NOT NULL PRIMARY KEY, 
+	type VARCHAR(32) NOT NULL, 
 	category VARCHAR(32) NOT NULL, 
-	status VARCHAR(32) NOT NULL, 
+	status VARCHAR(32), 
 	date_posted TIMESTAMP NOT NULL, 
 	content TEXT NOT NULL, 
-	admin_id BIGINT NOT NULL REFERENCES public.user,
+	admin_id BIGINT,
 	user_id BIGINT NOT NULL REFERENCES public.user
 );
 
-DROP TABLE public.note CASCADE;
+
 DROP TABLE public.request CASCADE;
